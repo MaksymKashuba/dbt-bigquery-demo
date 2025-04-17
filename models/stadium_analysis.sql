c46e9f487a68a3f8ef2a94243f04eae65670f864
MaksymKashuba <maksymkashuba2003@gmail.com>
2025-04-17 23:20:45 +0300

    added analysis

---
+++ models/stadium_analysis.sql
@@ -0,0 +1,46 @@
+WITH city_capacity_cte AS (
+    SELECT city,
+           AVG(capacity) AS avg_capacity
+    FROM {{ ref('stadium_cleaned') }}
+    GROUP BY city
+    WHERE capacity > 40000
+),
+filtered_stadiums_cte AS (
+    SELECT s.id,
+           s.name,
+           s.city,
+           s.capacity,
+           s.country
+    FROM {{ ref('stadium_cleaned') }} s
+    JOIN city_capacity_cte c
+    ON s.city = c.city
+),
+stadium_cte AS (
+    SELECT id,
+           name,
+           city,
+           capacity,
+           country,
+           events_hosted
+    FROM filtered_stadiums_cte
+    WHERE capacity > 30000
+),
+enhanced_stadium_cte AS (
+    SELECT id,
+           name,
+           city,
+           capacity,
+           country,
+           events_hosted,
+           (events_hosted / NULLIF(capacity, 0)) * 100 AS utilization_percentage
+    FROM stadium_cte
+    WHERE (events_hosted / NULLIF(capacity, 0)) * 100 > 50
+)
+SELECT id,
+       name,
+       city,
+       capacity,
+       country,
+       events_hosted,
+       utilization_percentage
+FROM enhanced_stadium_cte
