6a0bdb6c71898ae59b2efc52b117d3a31102c01f
MaksymKashuba <maksymkashuba2003@gmail.com>
2025-04-17 23:02:46 +0300

    quick fix x1

--- models/customer.sql
+++ models/customer.sql
@@ -2,14 +2,14 @@ WITH customers AS (
     SELECT id,
            first_name,
            last_name
-    FROM 'dbt-tutorial.jaffle_shop.customers'
+    FROM `dbt-tutorial.jaffle_shop.customers`
 ),
 orders AS (
     SELECT id,
            user_id,
            order_date,
            status
-    FROM 'dbt-tutorial.jaffle_shop.orders'
+    FROM `dbt-tutorial.jaffle_shop.orders`
 ),
 customer_orders AS (
     SELECT c.id AS customer_id,
