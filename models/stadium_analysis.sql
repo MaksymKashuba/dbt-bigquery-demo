387fcd58cfaf531dcd87483b3a74331e553b8105
MaksymKashuba <maksymkashuba2003@gmail.com>
2025-04-17 23:36:40 +0300

    small fix x3

--- models/stadium_analysis.sql
+++ models/stadium_analysis.sql
@@ -1,42 +1,19 @@
-WITH city_capacity_cte AS (
-    SELECT city,
-           AVG(capacity) AS avg_capacity
+WITH city_capacity AS (
+    SELECT city
     FROM {{ ref('stadium_cleaned') }}
     WHERE capacity > 40000
     GROUP BY city
-),
-filtered_stadiums_cte AS (
-    SELECT s.name,
-           s.city,
-           s.capacity,
-           s.country
-    FROM {{ ref('stadium_cleaned') }} s
-    JOIN city_capacity_cte c
-    ON s.city = c.city
-),
-stadium_cte AS (
-    SELECT name,
-           city,
-           capacity,
-           country,
-           events_hosted
-    FROM filtered_stadiums_cte
-    WHERE capacity > 30000
-),
-enhanced_stadium_cte AS (
-    SELECT name,
-           city,
-           capacity,
-           country,
-           events_hosted,
-           (events_hosted / NULLIF(capacity, 0)) * 100 AS utilization_percentage
-    FROM stadium_cte
-    WHERE (events_hosted / NULLIF(capacity, 0)) * 100 > 50
 )
-SELECT name,
-       city,
-       capacity,
-       country,
-       events_hosted,
-       utilization_percentage
-FROM enhanced_stadium_cte
+SELECT 
+    s.stadium,
+    s.city,
+    s.capacity,
+    s.country,
+    s.region,
+    s.rank,
+    s.images,
+    s.home_team,
+    s.location
+FROM {{ ref('stadium_cleaned') }} s
+JOIN city_capacity c ON s.city = c.city
+WHERE s.capacity > 30000
