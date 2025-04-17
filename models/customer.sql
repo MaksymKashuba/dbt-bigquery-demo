103c9553781c45f8b17e5002183a5a4c644bf809
MaksymKashuba <maksymkashuba2003@gmail.com>
2025-04-17 22:53:44 +0300

    Added customer.sql

---
+++ models/customer.sql
@@ -0,0 +1,32 @@
+WITH customers AS (
+    SELECT id,
+           first_name,
+           last_name
+    FROM 'dbt-tutorial.jaffle_shop.customers'
+),
+orders AS (
+    SELECT id,
+           user_id,
+           order_date,
+           status
+    FROM 'dbt-tutorial.jaffle_shop.orders'
+),
+customer_orders AS (
+    SELECT c.id AS customer_id,
+           c.first_name,
+           c.last_name,
+           o.id AS order_id,
+           o.order_date,
+           o.status
+    FROM customers c
+    LEFT JOIN orders o ON c.id = o.user_id
+)
+SELECT customer_id,
+       first_name,
+       last_name,
+       order_id,
+       order_date,
+       status
+FROM customer_orders
+WHERE order_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY)
+  AND status = 'completed'
